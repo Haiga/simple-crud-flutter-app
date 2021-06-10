44ab6852f851589d835ea052499644948fbe3bcd
Pedro <pedro_rga@hotmail.com>
2021-06-10 16:27:50 -0300

    adding Providers

--- lib/providers/users.dart
+++ lib/providers/users.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:simple_crud_flutter_app/data/dummy_users.dart';
 import 'package:simple_crud_flutter_app/models/user.dart';
@@ -15,5 +17,32 @@ class Users with ChangeNotifier {
 
   User byIndex(int i) {
     return _items.values.elementAt(i);
+  }
+
+  void put(User user) {
+    if (null == user) {
+      return;
+    }
+
+    if (user.id.trim().isNotEmpty && _items.containsKey(user.id)) {
+      _items.update(
+          user.id,
+          (_) => User(
+              id: user.id,
+              email: user.email,
+              name: user.name,
+              avatarUrl: user.avatarUrl));
+    } else {
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+          id,
+          () => User(
+              id: id,
+              name: user.name,
+              email: user.email,
+              avatarUrl: user.avatarUrl));
+    }
+
+    notifyListeners();
   }
 }
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_crud_flutter_app/components/user_tile.dart';
+import 'package:simple_crud_flutter_app/models/user.dart';
 import 'package:simple_crud_flutter_app/providers/users.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Users users = Provider.of(context);
+    final Users users = Provider.of(context); //, listen: false
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuários'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                users.put(User(
+                    id: '', name: 'Ted', email: 'Ted@Ted.com', avatarUrl: ''));
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       body: ListView.builder(
           itemCount: users.count,
