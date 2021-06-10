d13aa0df8f4ac8e994589366eaa636a39ed12e8c
Pedro <pedro_rga@hotmail.com>
2021-06-10 15:47:57 -0300

    adding ListTile with circle avatar

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:simple_crud_flutter_app/components/user_tile.dart';
 import 'package:simple_crud_flutter_app/data/dummy_users.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const users = {...DUMMY_USERS};
+    final users = {...DUMMY_USERS};
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuários'),
       ),
       body: ListView.builder(
           itemCount: users.length,
-          itemBuilder: (ctx, i) => Text(users.values.elementAt(i).name)),
+          itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i))),
     );
   }
 }
